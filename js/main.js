99e8323e632fe4150ecb9b02bd16278b0108ee2d
Jamie Waters <jwaters627@gmail.com>
2016-04-11 13:15:41 -0400

    added age check

---
+++ js/main.js
@@ -0,0 +1,22 @@
+function sizer() {
+    var gk = $('#gatekeeper').innerHeight();
+
+    $('#box').css({ marginTop: gk * .2 });
+};
+sizer();
+window.addEventListener('resize', function() {
+    sizer()
+});
+
+
+// $('#yes').click(function(){
+// 	$('#gatekeeper').addClass('clicked', 1000);
+// });
+
+ $('#yes').on('click', function () {
+    $('#gatekeeper').fadeOut(1000);
+});
+
+$(function(){  // $(document).ready shorthand
+  $('#gatekeeper').fadeIn(3000);
+});
